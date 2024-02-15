04b349aba003f5c964b2bfbc0a4b21bdb78af7a4
bhakuparkharsohtun <bha.ksohtun@gmail.com>
2024-02-15 23:17:08 +0530

    first commit

---
+++ QCbreeze/Ribbon1.vb
@@ -0,0 +1,51 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+Imports Microsoft.Office.Core
+Imports Microsoft.Office.Interop.PowerPoint
+Imports System.Diagnostics
+Public Class Ribbon1
+
+    Private Sub Ribbon1_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As RibbonControlEventArgs) Handles Button1.Click
+        '  ' Get the current presentation
+        '  Dim pres As Presentation = Globals.ThisAddIn.Application.ActivePresentation
+        '
+        '  ' Loop through all the slides
+        '  For Each slide As Slide In pres.Slides
+        '
+        '      ' Loop through all the shapes on the slide
+        '      For Each shape As Microsoft.Office.Interop.PowerPoint.Shape In slide.Shapes
+        '
+        '          ' Check if the shape is hidden
+        '          If shape.Visible = MsoTriState.msoFalse Then
+        '
+        '              ' Do something with the hidden shape
+        '              ' For example, print its name to the debug window
+        '              Debug.WriteLine(shape.Name)
+        '
+        '          End If
+        '
+        '      Next
+        '
+        '  Next
+
+        Dim sld As PowerPoint.Slide
+        Dim shp As PowerPoint.Shape
+        Dim count As Integer
+        Dim msg As String 'declare a variable to store the message
+        count = 0
+        msg = "" 'initialize the message as an empty string
+        For Each sld In Globals.ThisAddIn.Application.ActivePresentation.Slides
+            For Each shp In sld.Shapes
+                If shp.Visible = Microsoft.Office.Core.MsoTriState.msoFalse Then
+                    count = count + 1
+                    msg = msg & "Slide " & sld.SlideIndex & ": " & shp.Name & " is hidden" & vbNewLine 'append the new information to the message with a new line
+                End If
+            Next shp
+        Next sld
+        MsgBox("Total number of hidden layers: " & count & vbNewLine & msg) 'display the final message with the count and the details
+    End Sub
+
+End Class
---
+++ QCbreeze/ThisAddIn.vb
@@ -0,0 +1,22 @@
+﻿Public Class ThisAddIn
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+        ' Add a handler for the PresentationOpen event.
+        AddHandler Application.PresentationOpen, AddressOf Application_PresentationOpen
+    End Sub
+
+    Private Sub Application_PresentationOpen(ByVal Pres As PowerPoint.Presentation)
+        ' Loop through all the slides in the presentation.
+        For Each sld As PowerPoint.Slide In Pres.Slides
+            ' Loop through all the shapes in the slide.
+            For Each shp As PowerPoint.Shape In sld.Shapes
+                ' Set the visible property of the shape to true.
+                shp.Visible = Office.MsoTriState.msoTrue
+            Next
+        Next
+    End Sub
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+End Class
